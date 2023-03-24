7eeef16edc9af2ed1ca338eeffb8fceaccff0b63
Leonardo M. Primieri <leo.primieri@gmail.com>
2023-03-24 19:42:44 -0300

    chore: initial structure

---
+++ lib/list_classes.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ListClasses extends StatefulWidget {
+  const ListClasses({super.key});
+
+  @override
+  State<ListClasses> createState() => _ListClassesState();
+}
+
+class _ListClassesState extends State<ListClasses> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('My Schedul2e'),
+        ),
+        body: Center(
+          child: Column(
+            children: [],
+          ),
+        ));
+  }
+}
